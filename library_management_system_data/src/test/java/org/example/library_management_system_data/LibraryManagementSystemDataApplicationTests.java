0b4c741b42209946f642dc76e2313ef64503b727
eman.mabrouk <eman.mohamed3061@gmail.com>
2025-02-20 11:36:15 +0200

    Resolved merge conflicts

---
+++ library_management_system_data/src/test/java/org/example/library_management_system_data/LibraryManagementSystemDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.library_management_system_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
