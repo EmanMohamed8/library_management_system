6e19492011a5ab000210e55edee5413544ea07ee
eman.mabrouk <eman.mohamed3061@gmail.com>
2025-05-25 12:35:10 +0300

    add dates in base entity

--- library_management_system_data/src/main/java/org/example/library_management_system_data/models/BaseEntity.java
+++ library_management_system_data/src/main/java/org/example/library_management_system_data/models/BaseEntity.java
@@ -5,6 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,6 +19,14 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime modifiedDate;
 
     public Long getId() {
         return id;
